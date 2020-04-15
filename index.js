80b67b6d0bfcad9eb6ce5ddb1ba732b49c5149a6
Serena Hasler <srhasler@yahoo.com>
2020-04-15 14:42:40 -0500

    Add functions for 'subtractor', 'multiplier' and 'divider' - each just takes in 2 NUMBERS.

---
+++ index.js
@@ -0,0 +1,27 @@
+function adder(x, y) {
+  return x + y;
+}
+
+// TODO: Add functions for 'subtractor', 'multiplier' and 'divider' - each just takes in 2 NUMBERS.
+function subtractor(x, y) {
+  return x - y;
+}
+
+function multiplier(x, y) {
+  return x * y;
+}
+
+function divider(x, y) {
+  return x / y;
+}
+
+/**
+ * 'calculate' is specifying 3 NAMED PARAMETERS.
+ *
+ * TODO: REFACTOR this so that it does this via OBJECT DESTRUCTURING.
+ * No DEFAULT VALUES needed.
+ *
+ */
+function calculate(mathFxn, num1, num2) {
+  // TODO: RETURN the result of INVOKING 'mathFxn' with 'num1' and 'num2' as the ARGUMENTS.
+}
