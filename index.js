9327e77ea55f59c76747306f77a5276e070b6781
Serena Hasler <srhasler@yahoo.com>
2020-04-18 11:24:23 -0500

    Add return

--- index.js
+++ index.js
@@ -1,20 +1,16 @@
 function adder(x, y) {
   return x + y;
 }
-
 // TODO: Add functions for 'subtractor', 'multiplier' and 'divider' - each just takes in 2 NUMBERS.
 function subtractor(x, y) {
   return x - y;
 }
-
 function multiplier(x, y) {
   return x * y;
 }
-
 function divider(x, y) {
   return x / y;
 }
-
 /**
  * 'calculate' is specifying 3 NAMED PARAMETERS.
  *
@@ -24,4 +20,13 @@ function divider(x, y) {
  */
 function calculate(mathFxn, num1, num2) {
   // TODO: RETURN the result of INVOKING 'mathFxn' with 'num1' and 'num2' as the ARGUMENTS.
+  return mathFxn(num1, num2);
+
+  //return `${mathFxn(num1, num2)}`;
 }
+console.log(calculate(adder, 10, 2));
+console.log(calculate(subtractor, 10, 2));
+console.log(calculate(multiplier, 10, 2));
+console.log(calculate(divider, 10, 2));
+
+//console.log(calculate({ mathFxn: adder, num1: 10, num2: 2 }));
