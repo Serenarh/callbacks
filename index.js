4427b9e48335d430d3cb74c9542bf4826acdd32d
Serena Hasler <srhasler@yahoo.com>
2020-04-18 11:32:12 -0500

    Add fix to destructure properly

--- index.js
+++ index.js
@@ -18,15 +18,14 @@ function divider(x, y) {
  * No DEFAULT VALUES needed.
  *
  */
-function calculate(mathFxn, num1, num2) {
+function calculate({ mathFxn, num1, num2 }) {
   // TODO: RETURN the result of INVOKING 'mathFxn' with 'num1' and 'num2' as the ARGUMENTS.
-  return mathFxn(num1, num2);
+  //return mathFxn(num1, num2);
 
-  //return `${mathFxn(num1, num2)}`;
+  return `${mathFxn(num1, num2)}`;
 }
-console.log(calculate(adder, 10, 2));
-console.log(calculate(subtractor, 10, 2));
-console.log(calculate(multiplier, 10, 2));
-console.log(calculate(divider, 10, 2));
 
-//console.log(calculate({ mathFxn: adder, num1: 10, num2: 2 }));
+console.log(calculate({ mathFxn: adder, num1: 10, num2: 2 }));
+console.log(calculate({ mathFxn: subtractor, num1: 10, num2: 2 }));
+console.log(calculate({ mathFxn: divider, num1: 10, num2: 2 }));
+console.log(calculate({ mathFxn: multiplier, num1: 10, num2: 2 }));
